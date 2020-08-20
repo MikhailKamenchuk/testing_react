aa199054c8e2fe767e50fa1c9026ee8cb2380c89
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-20 17:59:20 +0300

    added lesson3 task3

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import Comment from './Comment.jsx';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQVK7xUD2P-8Dqt-CDb1JLDHzE_2yAns-AttA&usqp=CAU'
+}
+
+ReactDOM.render(
+  <Comment 
+    user={userInfo}
+    text="Good Job"
+    date={new Date()}
+  />,
+  document.getElementById('root')
+);
+
