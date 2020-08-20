201925034fb87fd9b8186972623319337ce8e6e8
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-20 14:33:17 +0300

    added lesson2 hw1

---
+++ lesson2/hw1/todo-list/review_build/precache-manifest.1fc07e019698115f111dee4cd91feaad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed8dc6ef24185f55092e5cfe6026836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e7c270e9af276ee2d3",
+    "url": "/static/css/main.3acbdcdb.chunk.css"
+  },
+  {
+    "revision": "f4cd8603cbc98fa8da98",
+    "url": "/static/js/2.730fe4ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.730fe4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e7c270e9af276ee2d3",
+    "url": "/static/js/main.10c8d410.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
