a63b788f76f64c7371f49599103104e1c3554c09
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-24 17:48:42 +0300

    added lesson7 task1

---
+++ lesson7/task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NumbersList from './NumbersList';
+
+const App = () => {
+  return(
+    <>
+      <NumbersList numbers={[1,2,3,4,5,6]}/>
+    </>
+  )
+}
+
+export default App
---
+++ lesson7/task2/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Mailbox from './Mailbox';
+
+const App = () => {
+  return(
+    <>
+      <Mailbox unreadMessages={[1]}/>
+    </>
+  )
+}
+
+export default App
