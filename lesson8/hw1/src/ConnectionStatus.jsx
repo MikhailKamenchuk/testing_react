80ea0f394160466c223f6b5867f437cf1a4b320a
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-25 17:20:06 +0300

    added lesson8 hw1

---
+++ lesson8/hw1/src/ConnectionStatus.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class ConnectionStatus extends React.Component {
+  state = {
+    status: 'online'
+  }
+
+  componentDidMount() {
+    window.addEventListener('online', this.toggleStatus);
+    window.addEventListener('offline', this.toggleStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.toggleStatus);
+    window.removeEventListener('offline', this.toggleStatus);
+  }
+
+  toggleStatus = () => {
+    const newStatus = this.state.status === 'online' ? 'offline' : 'online';
+    this.setState({
+      status: newStatus
+    })
+  }
+
+  render() {
+    const {status} = this.state;
+    const classNames = status !== 'online' ? "status status_offline" : "status";
+    return <div className={classNames}>{status}</div>
+  }
+}
+
+export default ConnectionStatus
