6160bcabc542ae4e388b6165b2ba08bc3505c0c5
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-26 08:24:41 +0300

    some changes lesson9 task2

--- lesson9/task2/src/App.jsx
+++ lesson9/task2/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
-import ConnectionStatus from './ConnectionStatus';
+import UserForm from './UserForm';
 
-const App = () => {
-  return(
-    <>
-      <ConnectionStatus />
-    </>
-  )
+class App extends React.Component {
+
+  createUser = user => console.log(user);
+
+  render() {
+    return (
+      <>
+        <UserForm onSubmit={this.createUser} />
+      </>
+    )
+  }
 }
 
 export default App
