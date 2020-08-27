1edd45a6c5cc1c7661e3ec4154c72e7ff2923f25
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-27 10:11:48 +0300

    added lesson11 task2

--- lesson11/task2/src/App.jsx
+++ lesson11/task2/src/App.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
-import UserForm from './UserForm';
+import UserForm from './Dialog';
+import Dialog from './Dialog';
 
 class App extends React.Component {
+  state = {
+    isOpen: false
+  }
 
-  createUser = user => console.log(user);
+  closeDialog = () => this.setState({
+    isOpen: false
+  })
+
+  showDialog = () => this.setState({
+    isOpen: true
+  })
 
   render() {
     return (
-      <>
-        <UserForm onSubmit={this.createUser} />
-      </>
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>Show dialog</button>
+
+        <Dialog
+          title='Recommendation'
+          isOpen={this.state.isOpen}
+          onClose={this.closeDialog}
+        >
+          <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
+        </Dialog>
+      </div>
     )
   }
 }
