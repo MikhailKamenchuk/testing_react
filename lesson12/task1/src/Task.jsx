8052dbcc2c863f16f1a9e410cbaaf4fd96c7e116
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-28 09:25:21 +0300

    refactored lesson12 task1

--- lesson12/task1/src/Task.jsx
+++ lesson12/task1/src/Task.jsx
@@ -2,39 +2,24 @@ import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Task = ({ id, text, done, onChange, onDelete }) => {
-  const listItemClasses = classNames("list-item", { "list-item_done": done })
-  return (
-    <li className={listItemClasses}>
-      <input
-        type="checkbox"
-        className="list-item__checkbox"
-        defaultChecked={done}
-        onChange={() => onChange(id)}
-      />
-      <span className="list-item__text">{text}</span>
-      <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
-    </li>
-  )
+class Task extends React.PureComponent {
+  render(){
+    const { id, text, done, onChange, onDelete } = this.props;
+    const classes = classNames("list-item", { "list-item_done": done })
+    return (
+      <li className={classes}>
+        <input
+          type="checkbox"
+          className="list-item__checkbox"
+          defaultChecked={done}
+          onChange={() => onChange(id)}
+        />
+        <span className="list-item__text">{text}</span>
+        <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
+      </li>
+    )
+  }
 };
-// class Task extends React.PureComponent {
-//   render(){
-//     const { id, text, done, onChange, onDelete } = this.props;
-//     const classes = classNames("list-item", { "list-item_done": done })
-//     return (
-//       <li className={classes}>
-//         <input
-//           type="checkbox"
-//           className="list-item__checkbox"
-//           defaultChecked={done}
-//           onChange={() => onChange(id)}
-//         />
-//         <span className="list-item__text">{text}</span>
-//         <button className="list-item__delete-btn" onClick={() => onDelete(id)}></button>
-//       </li>
-//     )
-//   }
-// };
 
 // Task.propTypes = {
 //   id: PropTypes.string, 
