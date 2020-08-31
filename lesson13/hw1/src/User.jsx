133330393762861a18dc1e3b0a59269aa95ca4f9
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-31 08:22:29 +0300

    some refactoring in lesson13 hw1

--- lesson13/hw1/src/User.jsx
+++ lesson13/hw1/src/User.jsx
@@ -5,16 +5,27 @@ class User extends React.PureComponent {
     userData: null
   }
 
+  fetchUserData = userId => fetch(`https://api.github.com/users/${userId}`)
+  .then(res => res.json())
+  .then(userData => this.setState({ userData }))
+
   componentDidMount() {
-    const { userId } = this.props.match.params
-    fetch(`https://api.github.com/users/${userId}`)
-      .then(res => res.json())
-      .then(userData => this.setState({ userData }))
+    const { userId } = this.props.match.params;
+    this.fetchUserData(userId)    
+  }
+
+  componentDidUpdate(prevProps) {
+    const { userId } = this.props.match.params;
+    if (userId !== prevProps.match.params.userId) {
+      this.fetchUserData(userId);
+    }
   }
 
   render() {
     if (!this.state.userData) return null;
+    
     const { avatar_url, name, location } = this.state.userData;
+    
     return (
       <div className="user">
         <img alt="User Avatar" src={avatar_url} className="user__avatar" />
--- lesson13/hw1/webpack.config.js
+++ lesson13/hw1/webpack.config.js
@@ -8,7 +8,8 @@ module.exports = (env, argv) => {
   const config = {
     entry: "./src/index.jsx",
     output: {
-      filename: "bundle.js"
+      filename: "bundle.js",
+      publicPath: '/'
     },
     module: {
       rules: [
