2b8a9b991a9a03be2e7322e821ca2e57493f2014
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-31 10:01:48 +0300

    some refactoring lesson14 task2

--- lesson14/task2/src/App.jsx
+++ lesson14/task2/src/App.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-import Home from './Home';
-import Users from './Users';
+import User from './User';
 
 const App = () => (
   <div className="page">
     <BrowserRouter>
-      <ul className="navigation">
-        <li className="navigation__item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navigation__item">
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/" >
-          <Home />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-      </Switch>
+      <div className="page__content">
+        <h1>Users</h1>
+        <ul className="navigation">
+          <li className="navigation__item">
+            <Link to="/users/github">Github</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/users/facebook">Facebook</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/users" exact>
+            <span>Select a user please</span>
+          </Route>
+          <Route path="/users/:userId" >
+            <User />
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   </div>
 )
