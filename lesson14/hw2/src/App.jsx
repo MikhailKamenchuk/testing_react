2835776430d3257f442005ce699b27884f3c89b1
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-31 13:22:38 +0300

    added lesson14 hw2

--- lesson14/hw2/src/App.jsx
+++ lesson14/hw2/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import Counter from './Counter';
+import Clock from './Clock';
 
-const App = () => (
-  <Counter />
-)
+const App = () => {
+  const [visible, toggleVisible] = useState(true);
+
+  return (
+    <>
+      <button className="btn" onClick={() => toggleVisible(!visible)}>Hide Clocks</button>
+      {visible && <>
+        <Clock location={'New York'} offset={-5} />
+        <Clock location={'Kyiv'} offset={2} />
+        <Clock location={'London'} offset={0} />
+      </>}
+    </>
+  )
+}
 
 export default App
